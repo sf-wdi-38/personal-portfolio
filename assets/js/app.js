c2efbb8e2360b98aa11d27cc9876de0f9a265896
Kat Tsukuda <BearHeartsBat@Kats-MacBook-Pro.local>
2017-05-09 11:30:48 -0700

    first edits to index.html, main.css, and app.jsl

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,47 @@
 // sanity check
 console.log("app.js linked.");
+$(".slider").slick({
+
+  // normal options...
+  infinite: false,
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
+
+// On swipe event
+$('.your-element').on('swipe', function(event, slick, direction){
+  console.log(direction);
+  // left
+});
+
+// On edge hit
+$('.your-element').on('edge', function(event, slick, direction){
+  console.log('edge was hit')
+});
+
+// On before slide change
+$('.your-element').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  console.log(nextSlide);
+});
