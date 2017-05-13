a4a6421e9ae8ffabc013f99f5a76a5d7ffe28a0d
justinmcdavid <jmcdavid@gmail.comgit config --global user.email jmcdavid@gmail.com>
2017-05-12 20:37:16 -0700

    WIP: Adding sections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,58 @@
 // sanity check
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+  let contDesign = [
+    {
+      title: "Some Title for this Design Content. - 1",
+      description: "First long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "design_image_1.jpg"
+    },
+    {
+      title: "Some Title for this Design Content. - 2",
+      description: "Second long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "design_image_2.jpg"
+    },
+    {
+      title: "Some Title for this Design Content. - 3",
+      description: "Third long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "design_image_3.jpg"
+    }
+  ];
+
+  let contThought = [
+    {
+      title: "Some Title for this Thought Content. - 1",
+      description: "First long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "thought_image_1.jpg"
+    },
+    {
+      title: "Some Title for this Thought Content. - 2",
+      description: "Second long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "thought_image_2.jpg"
+    },
+    {
+      title: "Some Title for this Thought Content. - 3",
+      description: "Third long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "thought_image_3.jpg"
+    }
+  ];
+
+  let contDev = [
+    {
+      title: "Some Title for this Dev Content. - 1",
+      description: "First long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "dev_image_1.jpg"
+    },
+    {
+      title: "Some Title for this Dev Content. - 2",
+      description: "Second long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "dev_image_2.jpg"
+    },
+    {
+      title: "Some Title for this Dev Content. - 3",
+      description: "Third long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
+      pic: "dev_image_3.jpg"
+    }
+  ];
+  console.log( "ready!" );
 });
