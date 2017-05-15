ee147a6f5c016c4e77a0dbd0d0636878f7ab0740
justinmcdavid <jmcdavid@gmail.comgit config --global user.email jmcdavid@gmail.com>
2017-05-14 18:15:11 -0700

    Adding some test images to play with

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,37 +5,37 @@ $( document ).ready(function() {
     {
       title: "Some Title for this Design Content. - 1",
       description: "First long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_1.jpg"
+      pic: "assets/imgs/design_1.png"
     },
     {
       title: "Some Title for this Design Content. - 2",
       description: "Second long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_2.jpg"
+      pic: "assets/imgs/design_2.png"
     },
     {
       title: "Some Title for this Design Content. - 3",
       description: "Third long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_3.jpg"
+      pic: "assets/imgs/design_3.png"
     },
     {
       title: "Some Title for this Design Content. - 4",
       description: "Fourth long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_4.jpg"
+      pic: "assets/imgs/design_4.png"
     },
     {
       title: "Some Title for this Design Content. - 5",
       description: "Fifth long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_5.jpg"
+      pic: "assets/imgs/design_5.png"
     },
     {
       title: "Some Title for this Design Content. - 6",
       description: "Sixth long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_6.jpg"
+      pic: "assets/imgs/design_6.png"
     },
     {
       title: "Some Title for this Design Content. - 7",
       description: "Seventh long bit of design content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/design_image_7.jpg"
+      pic: "assets/imgs/design_7.png"
     }
   ];
 
@@ -43,32 +43,32 @@ $( document ).ready(function() {
     {
       title: "Some Title for this Thought Content. - 1",
       description: "First long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_1.jpg"
+      pic: "assets/imgs/thought_1.png"
     },
     {
       title: "Some Title for this Thought Content. - 2",
       description: "Second long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_2.jpg"
+      pic: "assets/imgs/thought_2.png"
     },
     {
       title: "Some Title for this Thought Content. - 3",
       description: "Third long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_3.jpg"
+      pic: "assets/imgs/thought_3.png"
     },
     {
-      title: "Some Title for this Thought Content. - 1",
+      title: "Some Title for this Thought Content. - 4",
       description: "Fourth long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_4.jpg"
+      pic: "assets/imgs/thought_4."
     },
     {
-      title: "Some Title for this Thought Content. - 2",
+      title: "Some Title for this Thought Content. - 5",
       description: "Fifth long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_5.jpg"
+      pic: "assets/imgs/thought_5.png"
     },
     {
-      title: "Some Title for this Thought Content. - 3",
+      title: "Some Title for this Thought Content. - 6",
       description: "Sixth long bit of thought content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/thought_image_6.jpg"
+      pic: "assets/imgs/thought_6.png"
     }
   ];
 
@@ -76,47 +76,47 @@ $( document ).ready(function() {
     {
       title: "Some Title for this Dev Content. - 1",
       description: "First long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_1.jpg"
+      pic: "assets/imgs/dev_1.png"
     },
     {
       title: "Some Title for this Dev Content. - 2",
       description: "Second long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_2.jpg"
+      pic: "assets/imgs/dev_2.png"
     },
     {
       title: "Some Title for this Dev Content. - 3",
       description: "Third long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_3.jpg"
+      pic: "assets/imgs/dev_3.png"
     },
     {
       title: "Some Title for this Dev Content. - 4",
       description: "Fourth long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_4.jpg"
+      pic: "assets/imgs/dev_4.png"
     },
     {
       title: "Some Title for this Dev Content. - 5",
       description: "Fifth long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_5.jpg"
+      pic: "assets/imgs/dev_5.png"
     },
     {
       title: "Some Title for this Dev Content. - 6",
       description: "Sixth long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_6.jpg"
+      pic: "assets/imgs/dev_6.png"
     },
     {
       title: "Some Title for this Dev Content. - 7",
       description: "Seventh long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_7.jpg"
+      pic: "assets/imgs/dev_7.png"
     },
     {
       title: "Some Title for this Dev Content. - 8",
       description: "Eighth long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_8.jpg"
+      pic: "assets/imgs/dev_8.png"
     },
     {
       title: "Some Title for this Dev Content. - 9",
       description: "Ninth long bit of development content where I drone on and on about this project and something something until you lose interest and click back to a listicle of some sort.",
-      pic: "assets/imgs/dev_image_9.jpg"
+      pic: "assets/imgs/dev_9.png"
     },
   ];
   console.log( "ready!" );
